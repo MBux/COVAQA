4e267462e5d1aad7e4be177ff7ade6af2cd27d49
Mike Buxton <bux@pdx.edu>
2021-06-09 17:06:52 -0700

    More Home screen additions (used st.write)

--- src/home.py
+++ src/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 def app():
     st.title('Home')
     st.subheader('**COVAQA** COVID-19 Vaccination Dashboard and Question Answering Interface')
-    st.markdown('COVAQA is an open-source, web-based COVID-19 dashboard that provides up-to-date United States COVID-19 data for various metrics; including but not limited to: new cases by state, dominant variant by state, new cases by county, and more. In addition to the dashboard, COVAQA also features a COVID-19 Question Answering (QA) chat-bot. :robot:')
+    st.write('COVAQA is an open-source, web-based COVID-19 dashboard that provides up-to-date United States COVID-19 data for various metrics; including but not limited to: new cases by state, dominant variant by state, new cases by county, and more. In addition to the dashboard, COVAQA also features a COVID-19 Question Answering (QA) chat-bot. :robot:')
     st.text('COVAQA started as a Spring 2021 Portland State University Computer Science and Engineering Natural Language Processing and Mobile Health Project')
-    st.markdown('COVAQA Authors: [Mike Buxton :octocat:](https://github.com/MBux), [Jonathan Bral :octocat:](https://github.com/bralpdx), [Cosimo Gonnelli :octocat:](https://github.com/cosimogonnelli), and [Anthony Bobrovnikov :octocat:](https://github.com/anthonybob)')
-    st.markdown('[GitHub Repo](https://github.com/MBux/COVAQA)')
+    st.write('COVAQA Authors: [Mike Buxton :octocat:](https://github.com/MBux), [Jonathan Bral :octocat:](https://github.com/bralpdx), [Cosimo Gonnelli :octocat:](https://github.com/cosimogonnelli), and [Anthony Bobrovnikov :octocat:](https://github.com/anthonybob)')
+    st.write('[GitHub Repo](https://github.com/MBux/COVAQA)')
