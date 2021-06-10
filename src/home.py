f3f8620b74651e231e8c5c2b67dc752bfdcc4aaf
Mike Buxton <bux@pdx.edu>
2021-06-09 17:01:41 -0700

    Added some stuff to the home screen

--- src/home.py
+++ src/home.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def app():
     st.title('Home')
-
-    st.write('Placeholder')
+    st.subheader('**COVAQA** COVID-19 Vaccination Dashboard and Question Answering Interface')
+    st.write('COVAQA is an open-source, web-based COVID-19 dashboard that provides up-to-date United States COVID-19 data for various metrics; including but not limited to: new cases by state, dominant variant by state, new cases by county, and more. In addition to the dashboard, COVAQA also features a COVID-19 Question Answering (QA) chat-bot. :robot:')
